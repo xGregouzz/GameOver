c52ab0d28bd2ba4ed50faa9fc9e2468d8aa86ede
Elie Sigal <sigalelie03@gmail.com>
2021-05-20 21:28:03 +0200

    index changement

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <?php 	
+	require_once ("data/database.php");
+	require_once ("config.php");
 
-	require_once ("database.php");
-	$db = connectToDatabase();
-	$database = recupdonnee($db);
-
-	if (isset($_GET["action"])) {
-		switch ($_GET["action"]) {
+try {
+	$connection = new PDO($dsn, $username, $password);
+	echo ("connecté");
+} catch (PDOException $error) {
+	echo $sql . "<br>" . $error->getMessage();
+}
+ 
+/*
+	// $db = connectToDatabase();
+	// $database = recupdonnee($db);
+*/
+	if (isset($_POST["action"])) {
+		switch ($_POST["action"]) {
 			case "stock.php":
 				$content_for_layout ="stock.php"; 
 				break;
 			case "connexion.php":
 				$content_for_layout ="connexion.php"; 
+				break;
 			default: 
 				$content_for_layout ="stock.php";
+				break;
 		}
-		$layout = "accueil.php";
+		$layout = "public/accueil.php";
 	} else {
 		$content_for_layout = "stock.php";
-		$layout = "accueil.php";
+		$layout = "public/accueil.php";
 	}	
 
-	include($layout);
+	include($layout); 
 ?>
