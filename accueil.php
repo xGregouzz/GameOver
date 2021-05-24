2a0f449a6f98e0ec3702a11c89b1e9a5c0c26b8c
matteo <759matteo@gmail.com>
2021-05-24 23:04:43 +0200

    css

--- accueil.php
+++ accueil.php
@@ -43,12 +43,52 @@
             <a href="http://localhost/phpmyadmin/index.php?route=/sql&db=gameover&table=articles&pos=0">Modifier Article</a>
     </div>
     </fieldset>
-    <nav class="links" style="--items: 5;">
+    <!-- <nav class="links" style="--items: 5;">
         <a href="#">Playstation</a>
         <a href="#">Xbox</a>
         <a href="#">Nintendo</a>
         <a href="#">Pc</a>
-    </nav>
+    </nav> -->
+    <link href="categories.css" rel="stylesheet">
+    <nav class="menu">
+		<section class="categorie">
+			<h3>Playstation</h3>
+			<ul>
+				<li><a href="#">Tagliatelle</a></li>
+				<li><a href="#">Fettuccine</a></li>
+				<li><a href="#">Lasagne</a></li>
+			</ul>
+		</section>
+		<section class="categorie">
+			<h3>Xbox</h3>
+			<ul>
+				<li><a href="#">Spaghetti</a></li>
+				<li><a href="#">Spaghettoni</a></li>
+				<li><a href="#">Vermicelli</a></li>
+				<li><a href="#">Capellini</a></li>
+			</ul>
+		</section>
+		<section class="categorie">
+			<h3>Nintendo</h3>
+			<ul>
+				<li><a href="#">Macaroni</a></li>
+				<li><a href="#">Cannelloni</a></li>
+				<li><a href="#">Penne</a></li>
+				<li><a href="#">Cavattapi</a></li>
+			</ul>
+		</section>
+		<section class="categorie">
+			<h3>PC</h3>
+			<ul>
+				<li><a href="#">Farfalle</a></li>
+				<li><a href="#">Fusilli</a></li>
+				<li><a href="#">Ravioli</a></li>
+				<li><a href="#">Gnocchi</a></li>
+				<li><a href="#">Conchigliette</a></li>
+			</ul>
+		</section>
+	</nav>
+    
 </header>
 
 </body>
