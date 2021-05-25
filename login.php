f2a1cddb545aa09f521620e7d7a08ebf9b63b0f4
matteo <759matteo@gmail.com>
2021-05-25 11:48:34 +0200

    session

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
 				$data = $stmt->fetch(PDO::FETCH_ASSOC);
 
 				if($data == false) {
-					$errMsg = "l'utilisateur $mail n'existe pas.";
+					$errMsg = "L'adresse mail ou le mot de passe est incorrect.";
 				} else {
 					if($mdp == $data['mdp']) {
 						$_SESSION['nom'] = $data['nom'];
@@ -59,7 +59,7 @@
 			?>
 			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Connexion</b></div>
 			<div style="margin: 15px">
-				<form action="connexion.php" method="post">
+				<form action="" method="post">
 					Email : <input type="text" name="mail" value="<?php if(isset($_POST['mail'])) echo $_POST['mail'] ?>" autocomplete="off" class="box"/><br /><br />
 					Mot de Passe : <input type="password" name="mdp" value="<?php if(isset($_POST['mdp'])) echo $_POST['mdp'] ?>" autocomplete="off" class="box" /><br/><br />
 					<input type="submit" name='login' value="Se connecter" class='submit'/><br />
