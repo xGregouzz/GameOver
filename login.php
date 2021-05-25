70f37eb32d816e236f5d13104a65689df74b7c84
matteo <759matteo@gmail.com>
2021-05-25 11:58:44 +0200

    session

--- login.php
+++ login.php
@@ -27,10 +27,10 @@
 						$_SESSION['mail'] = $data['mail'];
 						$_SESSION['mdp'] = $data['mdp'];
 
-						header('Location: accueil.php');
+						header('Location: accueil_membre.php');
 						exit;
 					} else {
-						$errMsg = 'Le mot de passe ne correspond pas.';
+						$errMsg = "L'adresse mail ou le mot de passe est incorrect.";
 					}
 				}
 			}
@@ -59,6 +59,7 @@
 			?>
 			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Connexion</b></div>
 			<div style="margin: 15px">
+
 				<form action="" method="post">
 					Email : <input type="text" name="mail" value="<?php if(isset($_POST['mail'])) echo $_POST['mail'] ?>" autocomplete="off" class="box"/><br /><br />
 					Mot de Passe : <input type="password" name="mdp" value="<?php if(isset($_POST['mdp'])) echo $_POST['mdp'] ?>" autocomplete="off" class="box" /><br/><br />
