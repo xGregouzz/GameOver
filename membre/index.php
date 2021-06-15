6843e00fc9ff8d799505df75c78ee77cf03c1ba1
Elie Sigal <sigalelie03@gmail.com>
2021-06-15 10:43:50 +0200

    rajout dans l'index

--- membre/index.php
+++ membre/index.php
@@ -53,6 +53,54 @@ require_once '../config.php';
 			case "supprimer_commandes.php":
 				$page ="supprimer_commandes.php"; 
 				break;
+			case 'accueil.php':
+				$page = "accueil.php";
+				break;
+			
+			case 'cart.php':
+				$page = 'cart.php';
+				break;
+			case 'addToCart':
+				if (isset($_GET["id"])) {
+					include_once("cart.php");
+					addToCart($_GET["id"], $connect);
+					$page = "panier.php";
+				} else {
+					$page = "accueil_membre.php";
+				}
+				break;
+			case 'checkout.php':
+				$page = "checkout.php";
+				break;
+			case 'login.php':
+				$page = "login.php";
+				break;
+			case 'panier.php':
+				$page = "panier.php";
+				break;
+			case 'unique_article.php':
+				$page = "register.php";
+				break;
+			case "empty.php":
+				include_once("./cart.php");
+				emptyCart();
+				$page = "showcart.php";
+				break;
+			case "saveorder":
+				if (isset($_POST["nom"]) AND isset($_POST["mail"]) AND isset($_POST["adresse"]) AND isset($_POST["mp"])) {
+					include_once("config.php");
+					$nom = $_POST['nom'];
+					$mail = $_POST['mail'];
+					$adr = $_POST['adresse'];
+					$mp = $_POST['mp'];
+					checkout($nom, $mail, $adr, $mp, $connect);
+					$page = "saveorder.php";
+					break;
+				} else {    
+					$page = "checkout.php";
+					break;
+				}   
+	
 			default: 
 				$page ="accueil_membre.php";
 		}
